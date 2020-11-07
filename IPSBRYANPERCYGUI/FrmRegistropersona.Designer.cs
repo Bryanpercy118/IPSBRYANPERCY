beba2dc8ecbb1a7b6c6ea89bbe3c84040dc51c16
PERSONAL <PERSONAL@DESKTOP-QP4UP8Q>
2020-11-06 23:53:10 -0500

    Mapeos del Registrar

--- IPSBRYANPERCYGUI/FrmRegistropersona.Designer.cs
+++ IPSBRYANPERCYGUI/FrmRegistropersona.Designer.cs
@@ -31,22 +31,24 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.Txtnliquidacion = new System.Windows.Forms.TextBox();
+            this.Txtidentificacion = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
-            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.Txtsalariopaciente = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
-            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.Txtvalorservicio = new System.Windows.Forms.TextBox();
             this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.combotipoafiliacion = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.txtsalario = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 65);
+            this.label1.Location = new System.Drawing.Point(12, 103);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(70, 13);
             this.label1.TabIndex = 0;
@@ -55,7 +57,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 107);
+            this.label2.Location = new System.Drawing.Point(13, 20);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(88, 13);
             this.label2.TabIndex = 1;
@@ -64,25 +66,25 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 25);
+            this.label3.Location = new System.Drawing.Point(12, 63);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(116, 13);
             this.label3.TabIndex = 2;
             this.label3.Text = "Numero de Liquidacion";
             // 
-            // textBox1
+            // Txtnliquidacion
             // 
-            this.textBox1.Location = new System.Drawing.Point(144, 25);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 3;
+            this.Txtnliquidacion.Location = new System.Drawing.Point(144, 63);
+            this.Txtnliquidacion.Name = "Txtnliquidacion";
+            this.Txtnliquidacion.Size = new System.Drawing.Size(100, 20);
+            this.Txtnliquidacion.TabIndex = 3;
             // 
-            // textBox2
+            // Txtidentificacion
             // 
-            this.textBox2.Location = new System.Drawing.Point(144, 65);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 4;
+            this.Txtidentificacion.Location = new System.Drawing.Point(144, 103);
+            this.Txtidentificacion.Name = "Txtidentificacion";
+            this.Txtidentificacion.Size = new System.Drawing.Size(100, 20);
+            this.Txtidentificacion.TabIndex = 4;
             // 
             // label4
             // 
@@ -93,12 +95,12 @@
             this.label4.TabIndex = 6;
             this.label4.Text = "Salario Paciente";
             // 
-            // textBox4
+            // Txtsalariopaciente
             // 
-            this.textBox4.Location = new System.Drawing.Point(144, 143);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(100, 20);
-            this.textBox4.TabIndex = 7;
+            this.Txtsalariopaciente.Location = new System.Drawing.Point(144, 143);
+            this.Txtsalariopaciente.Name = "Txtsalariopaciente";
+            this.Txtsalariopaciente.Size = new System.Drawing.Size(100, 20);
+            this.Txtsalariopaciente.TabIndex = 7;
             // 
             // label5
             // 
@@ -109,31 +111,22 @@
             this.label5.TabIndex = 8;
             this.label5.Text = "Valor del Servicio";
             // 
-            // textBox5
+            // Txtvalorservicio
             // 
-            this.textBox5.Location = new System.Drawing.Point(144, 189);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(100, 20);
-            this.textBox5.TabIndex = 9;
+            this.Txtvalorservicio.Location = new System.Drawing.Point(144, 189);
+            this.Txtvalorservicio.Name = "Txtvalorservicio";
+            this.Txtvalorservicio.Size = new System.Drawing.Size(100, 20);
+            this.Txtvalorservicio.TabIndex = 9;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(293, 25);
+            this.label6.Location = new System.Drawing.Point(256, 25);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(98, 13);
             this.label6.TabIndex = 10;
             this.label6.Text = "Salario Devengado";
             this.label6.Click += new System.EventHandler(this.label6_Click);
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(415, 25);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(35, 13);
-            this.label7.TabIndex = 11;
-            this.label7.Text = "label7";
             // 
             // button1
             // 
@@ -141,36 +134,65 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(100, 37);
             this.button1.TabIndex = 12;
-            this.button1.Text = "Registrar";
+            this.button1.Text = "Buscar";
             this.button1.UseVisualStyleBackColor = true;
             // 
-            // comboBox1
+            // combotipoafiliacion
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
+            this.combotipoafiliacion.FormattingEnabled = true;
+            this.combotipoafiliacion.Items.AddRange(new object[] {
             "Seleccione",
             "Contributivo ",
             "Subsidiado"});
-            this.comboBox1.Location = new System.Drawing.Point(144, 104);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(100, 21);
-            this.comboBox1.TabIndex = 13;
+            this.combotipoafiliacion.Location = new System.Drawing.Point(144, 17);
+            this.combotipoafiliacion.Name = "combotipoafiliacion";
+            this.combotipoafiliacion.Size = new System.Drawing.Size(100, 21);
+            this.combotipoafiliacion.TabIndex = 13;
+            this.combotipoafiliacion.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(28, 261);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 37);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Guardar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(259, 261);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 37);
+            this.button3.TabIndex = 15;
+            this.button3.Text = "Editar";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // txtsalario
+            // 
+            this.txtsalario.Location = new System.Drawing.Point(360, 22);
+            this.txtsalario.Name = "txtsalario";
+            this.txtsalario.Size = new System.Drawing.Size(103, 20);
+            this.txtsalario.TabIndex = 16;
             // 
             // FrmRegistropersona
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(475, 310);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.txtsalario);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.combotipoafiliacion);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
-            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.Txtvalorservicio);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.Txtsalariopaciente);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Txtidentificacion);
+            this.Controls.Add(this.Txtnliquidacion);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -186,15 +208,17 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox Txtnliquidacion;
+        private System.Windows.Forms.TextBox Txtidentificacion;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox Txtsalariopaciente;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox Txtvalorservicio;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox combotipoafiliacion;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox txtsalario;
     }
 }
